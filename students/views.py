0469aac386a5a5d204a7ac1bb9b9adba563dd2ab
pkw <pkw@biscuits.local>
2023-04-27 12:22:51 -0400

    add logic for student_list and detail_view

--- students/views.py
+++ students/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework import status
 
-# Create your views here.
+from .models import Student
+from .serializers import *
+
+@api_view(['GET', 'POST'])
+def student_list(request):
+    if request.method == 'GET':
+        data = Student.objects.all()
+        
+        serializer = StudentSerilizer(data, context={'request': request})
+        
+        return Response(serializer.data)
+        
+    elif request.method == 'POST':
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Resonse(status=HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT', 'DELETE'])
+def students_detail(request, pk):
+    try:
+        student = Student.objects.get(pk=pk)
+    except Student.DoesNot.Exist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'PUT':
+        serializer = StudentSerializer(student, data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serilzer.save()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        retun Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+    elif request.method == 'DELETE':
+        student.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
